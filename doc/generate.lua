d13ec16561f51e112ceb6c7753fb6a15e8766b55
Ofey Chan <ofey206@gmail.com>
2021-12-15 16:28:57 +0800

    PS: ./generate --lua2md

--- doc/generate.lua
+++ doc/generate.lua
@@ -4,24 +4,23 @@ local L_OPEN = '```lua'
 local L_CLOSE = '```'
 
 local function md2lua(it)
-    local L_IN = false
-    local LN = 0
+    local CODEBLOCK = false
+    local LINENUMBER = 0
 
     local ans = {"#!/bin/env lua\n\n"}
 
     for line in it do
-        LN = LN + 1
-        if (line == L_OPEN and not L_IN) or (line == L_CLOSE and L_IN) then
-            L_IN = not L_IN
-            table.insert(ans, "\n")
+        LINENUMBER = LINENUMBER + 1
+        if (line == L_OPEN and not CODEBLOCK) or (line == L_CLOSE and CODEBLOCK) then
+            CODEBLOCK = not CODEBLOCK
+            table.insert(ans, "-- "..line.."\n")
             goto continue
-        elseif (line == L_OPEN and L_IN) or (line == L_CLOSE and not L_IN) then
-            error(string.format("codeblock error in line %d", LN))
+        elseif (line == L_OPEN and CODEBLOCK) or (line == L_CLOSE and not CODEBLOCK) then
+            error(string.format("codeblock error in line %d", LINENUMBER))
         end
 
-        if L_IN then
-            table.insert(ans, line)
-            table.insert(ans, "\n")
+        if CODEBLOCK then
+            table.insert(ans, line.."\n")
         else
             table.insert(ans, "--")
             if line ~= "" then
@@ -34,4 +33,58 @@ local function md2lua(it)
     return table.concat(ans, "")
 end
 
-io.write(md2lua(io.lines()))
+local function starts_with(str, start)
+   return str:sub(1, #start) == start
+end
+
+local function lua2md(it)
+    local CODEBLOCK = false
+    local LINENUMBER = 0
+
+    local ans = {}
+
+    local first_line = true
+    for line in it do
+        if first_line then
+            if line == "#!/bin/env lua" then goto continue end
+            first_line = false
+        end
+        if (not CODEBLOCK) then
+        else
+        end
+        if (CODEBLOCK) then
+            if (line == "-- "..L_CLOSE) then
+                table.insert(ans, L_CLOSE.."\n")
+                CODEBLOCK = false
+            else
+                table.insert(ans, line.."\n")
+            end
+        else
+            if (line == "-- "..L_OPEN) then
+                table.insert(ans, L_OPEN.."\n")
+                CODEBLOCK = true
+            elseif (starts_with(line, "-- ")) then
+                table.insert(ans, line:sub(4, #line).."\n")
+            else
+                table.insert(ans, "\n")
+            end
+        end
+        ::continue::
+    end
+    return table.concat(ans, "")
+end
+
+
+if (arg[1]) then
+    if (arg[1] == "--lua2md") then
+        io.write(lua2md(io.lines()))
+    else
+        io.write([[Usage:
+    cat XXX.md | ./generate.lua > XXX.lua
+    cat XXX.lua | ./generate.lua --lua2md > XXX.md
+]])
+    end
+else
+    io.write(md2lua(io.lines()))
+end
+
